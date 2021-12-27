995d29309ff0049151d94bc9cb3e9bc273f40133
cries002 <christophries1@student.hs-rm.de>
2021-12-27 05:44:09 +0100

    added homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(new MyApp());
 
 class Pair<T1, T2> {
   final T1 a;
   final T2 b;
-
   Pair(this.a,this.b);
 }
 
@@ -16,70 +16,108 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
-      home: new ListPage(),
+      home: new start(),
     );
   }
 }
 
-class ListPage extends StatefulWidget {
-  const ListPage({Key? key}) : super(key: key);
+class start extends StatefulWidget {
+  const start({Key? key}) : super(key: key);
 
   @override
-  _ListPageState createState() => _ListPageState();
+  _startState createState() => _startState();
 }
 
-class _ListPageState extends State<ListPage> {
-  final topAppBar = AppBar(
-    elevation: 5,
-    backgroundColor: Color.fromRGBO(70, 70, 70, 1.0),
-    title: Text("Set Favourites"),
-    actions: <Widget>[
-      IconButton(
-        icon: Icon(Icons.list),
-        onPressed: () {},
-      )
-    ],
-  );
+List<Pair> favouriteList = [];
 
-  final makeBottom = Container(
-      height: 55.0,
-      child: BottomAppBar(
-          color: Colors.blueGrey,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.home, color: Colors.white)),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite, color: Colors.white)),
-            ],
-          )));
+int _selectedPage = 0;
+var pages = [home(favouriteList: favouriteList,),favourites()];
+String appBarText = "Home";
+Color _iconColorHome = Colors.indigo;
+Color _iconColorFav = Colors.white;
 
+class _startState extends State<start> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 5,
+          backgroundColor: Color.fromRGBO(70, 70, 70, 1.0),
+          title: Text(appBarText),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.list),
+              onPressed: () {},
+            )
+          ],
+        ),
+        bottomNavigationBar: Container(
+            height: 55.0,
+            child: BottomAppBar(
+                color: Colors.blueGrey,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            pages[0] = home(favouriteList: favouriteList,);
+                            _selectedPage = 0;
+                            appBarText = "Home";
+                            _iconColorHome = Colors.indigo;
+                            _iconColorFav = Colors.white;
+                          });
+                        },
+                        icon: Icon(Icons.home, color: _iconColorHome,size: 30,)),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _selectedPage = 1;
+                            appBarText = "Set Favourites";
+                            _iconColorFav = Colors.indigo;
+                            _iconColorHome = Colors.white;
+                          });
+                        },
+                        icon: Icon(Icons.favorite, color: _iconColorFav,size: 27)),
+                  ],
+                ))),
+        body: IndexedStack(
+          index: _selectedPage,
+          children: pages,
+        )
+    );
+  }
+}
+
+
+class favourites extends StatefulWidget {
+  const favourites({Key? key}) : super(key: key);
+
+  @override
+  _favouritesState createState() => _favouritesState();
+}
+
+class _favouritesState extends State<favourites> {
 
   Map<int,bool> itemSelected = Map();
 
   var teeList = [Pair("Grüner Tee", "5 Minuten"),Pair("Schwarzer Tee", "3 Minuten"),
-    Pair("Gelber Tee", "4 Minuten"),Pair("Kamille Tee", "2 Minuten"),
-    Pair("Weißer Tee", "5 Minuten"),Pair("Limonen Tee", "2 Minuten"),Pair("Apfel Tee", "2 Minuten")];
+    Pair("Rooibos Tee", "4 Minuten"),Pair("Kamille Tee", "2 Minuten"),
+    Pair("Marokanische Minze", "5 Minuten"),Pair("Limonen Tee", "2 Minuten"),Pair("Apfel Tee", "2 Minuten")];
 
   void fillItemSelected(){
     for (int i = 0; i < teeList.length; i++){
       itemSelected[i] = false;
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromRGBO(58, 66, 86, 1),
-        appBar: topAppBar,
-        bottomNavigationBar: makeBottom,
         body: Container(
           child: ListView.builder(
             scrollDirection: Axis.vertical,
-            shrinkWrap: true,
             itemCount: teeList.length,
             itemBuilder: (BuildContext context, int index) {
               if (itemSelected.isEmpty) fillItemSelected();
@@ -95,27 +133,32 @@ class _ListPageState extends State<ListPage> {
                         setState(() {
                           if (itemSelected[index] == false){
                             itemSelected[index] = true;
+                            favouriteList.add(teeList[index]);
                           }else{
                             itemSelected[index] = false;
+                            favouriteList.remove(teeList[index]);
                           }
                         });
                       },
                       contentPadding:
                       EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                       leading: Container(
-                        padding: EdgeInsets.only(right: 12),
+                        padding: EdgeInsets.only(top: 5,left: 5),
                         child: Icon(
                           itemSelected[index] == true ? Icons.favorite :
                           Icons.favorite_outline,
                           color: Colors.blueAccent
                         ),
                       ),
-                      title: Text(
-                        teeList[index].a,
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
+                      title: Container(
+                        padding: EdgeInsets.only(left: 20),
+                        child: Text(
+                            teeList[index].a,
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                          )
                       ),
                       subtitle: Container(
-                        padding: EdgeInsets.only(left: 0),
+                        padding: EdgeInsets.only(left: 20),
                           child: Row(
                             children: [
                               Icon(Icons.alarm, size: 15,),
@@ -131,3 +174,66 @@ class _ListPageState extends State<ListPage> {
     );
   }
 }
+
+class home extends StatefulWidget {
+  final List<Pair> favouriteList;
+
+  const home({required this.favouriteList, Key? key}) : super(key: key);
+
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1),
+        body: Container(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: favouriteList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                  elevation: 8.0,
+                  shape:
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  color: Colors.white,
+                  margin: new EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                  child: Container(
+                    child: ListTile(
+                        onTap: (){},
+                        contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        leading: Container(
+                          padding: EdgeInsets.only(top: 5,left: 5),
+                          child: Icon(Icons.favorite,
+                              color: Colors.blueAccent
+                          ),
+                        ),
+                        title: Container(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Text(
+                              favouriteList[index].a,
+                              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                            )
+                        ),
+                        subtitle: Container(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Row(
+                              children: [
+                                Icon(Icons.alarm, size: 15,),
+                                Text("  Dauer: " + favouriteList[index].b),
+                              ],
+                            )
+                        )
+                    ),
+                  ));
+            },
+          ),
+        )
+    );
+  }
+}
+
+
